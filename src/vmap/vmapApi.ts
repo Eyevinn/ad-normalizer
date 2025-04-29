f9e28d602bb2fc630dc583af2e010551a07c84d5
Fredrik Lundkvist <fredrik.lundkvist@eyevinn.se>
2025-04-29 09:53:42 +0200

    fix: lint
    Signed-off-by: Fredrik Lundkvist <fredrik.lundkvist@eyevinn.se>

--- src/vmap/vmapApi.ts
+++ src/vmap/vmapApi.ts
@@ -87,8 +87,14 @@ export const vmapApi: FastifyPluginCallback<AdApiOptions> = (
     async (req, reply) => {
       const path = req.url;
       const headers = req.headers;
-      const deviceUserAgent = getHeaderValue(headers, deviceUserAgentHeader.toLowerCase());
-      const forwardedFor = getHeaderValue(headers, 'X-Forwarded-For'.toLowerCase());
+      const deviceUserAgent = getHeaderValue(
+        headers,
+        deviceUserAgentHeader.toLowerCase()
+      );
+      const forwardedFor = getHeaderValue(
+        headers,
+        'X-Forwarded-For'.toLowerCase()
+      );
       let vmapReqHeaders = {};
       if (deviceUserAgent) {
         vmapReqHeaders = {
