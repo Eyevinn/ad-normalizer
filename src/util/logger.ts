7085428209b7ba4ed92ac16745157e3617b86f66
Fredrik Lundkvist <fredrik.lundkvist@eyevinn.se>
2025-04-29 13:40:04 +0200

    fix: lint
    Signed-off-by: Fredrik Lundkvist <fredrik.lundkvist@eyevinn.se>

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -2,9 +2,6 @@ import { createLogger, transports, format } from 'winston';
 
 export default createLogger({
   level: process.env.LOG_LEVEL ? process.env.LOG_LEVEL.toLowerCase() : 'info',
-  format: format.combine(
-    format.timestamp(),
-    format.json()
-  ),
+  format: format.combine(format.timestamp(), format.json()),
   transports: [new transports.Console()]
 });
